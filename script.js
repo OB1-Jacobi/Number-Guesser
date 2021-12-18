12d5f0cb11410f92836e676fcfe542c25715ba49
OB1-Jacobi <jpattimore@gmail.com>
2021-12-18 10:12:00 -0500

    updated js

--- script.js
+++ script.js
@@ -3,4 +3,37 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+    return Math.floor(Math.random() * 10);
+}
+
+const user = humanGuessInput.value;
+const computer = Math.floor(Math.random() * 10);
+const target = generateTarget();
+function compareGuesses(user, computer, target) {
+    userDiff = Math.abs(user - target);
+    computerDiff = Math.abs(computer - target);
+    if (userDiff > computerDiff) {
+        return false;
+    } else {
+        return true;
+    }
+}
+function updateScore("Winner") {
+    if(compareGuesses = false) {
+        computerScore += 1;
+        return "Computer Wins!"
+    }
+    else {
+        humanScore += 1;
+        return "User Wins!"
+    }
+}
+function advanceRound() {
+    currentRoundNumber += 1;
+    return "Next Round!"
+}
+
+updateScore("human");
+console.log(humanScore);
 
